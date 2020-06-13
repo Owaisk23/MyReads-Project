c27d5e64a91e6aebbdaa7ad9a51c76e56631926c
Owais Ahmed Khan <owais1566@gmail.com>
2020-06-13 03:53:49 +0000

    update build

---
+++ build/precache-manifest.2c765e85faf7b79ee8a36c6b315cf220.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9235aa5b484d9936118d",
+    "url": "/static/css/main.37f7aa4e.chunk.css"
+  },
+  {
+    "revision": "9235aa5b484d9936118d",
+    "url": "/static/js/main.9235aa5b.chunk.js"
+  },
+  {
+    "revision": "cded52de37b4142317b7",
+    "url": "/static/js/1.cded52de.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "6a1aa02e2a6c4fae528fa304f4c29bc5",
+    "url": "/index.html"
+  }
+];
